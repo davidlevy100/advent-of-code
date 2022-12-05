377123dd6fa2ed280139ad6ed4159aea88f52555
David Levy <david.isaac.levy@gmail.com>
2022-12-04 22:48:47 -0500

    day2

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	lines := []string{}
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	currElf := 0
+	//maxElf := 0
+	maxElfScore := 0
+	currScore := 0
+
+	scores := []int{}
+
+	for _, thisLine := range lines {
+
+		if thisLine == "" {
+
+			scores = append(scores, currScore)
+
+			if currScore > maxElfScore {
+				maxElfScore = currScore
+				//maxElf = currElf
+			}
+			currElf++
+			currScore = 0
+		} else {
+			val, _ := strconv.Atoi(thisLine)
+			currScore += val
+		}
+
+	}
+
+	scores = append(scores, currScore)
+
+	sort.Ints(scores)
+
+	total := 0
+
+	for _, val := range scores[(len(scores) - 3):] {
+		total += val
+	}
+
+	fmt.Printf("Part A answer: %d\n", scores[len(scores)-1])
+	fmt.Printf("Part B answer: %d\n", total)
+
 }
--- 2022/day2/main.go
+++ 2022/day2/main.go
@@ -1,7 +1,126 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var winner = map[string]string{
+	"A": "Y",
+	"B": "Z",
+	"C": "X",
+}
+
+var loser = map[string]string{
+	"A": "Z",
+	"B": "X",
+	"C": "Y",
+}
+
+var scores = map[string]int{
+	"A": 1,
+	"B": 2,
+	"C": 3,
+	"X": 1,
+	"Y": 2,
+	"Z": 3,
+}
+
+var scoresRev = map[int]string{
+	1: "A",
+	2: "B",
+	3: "C",
+}
+
+var names = map[string]string{
+	"A": "Rock",
+	"B": "Paper",
+	"C": "Scissors",
+	"X": "Rock",
+	"Y": "Paper",
+	"Z": "Scissors",
+}
 
 func main() {
-	fmt.Println("Hello, 世界")
+	lines, err := getInput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var partOneResult int
+
+	for _, thisLine := range lines {
+		plays := strings.Fields(thisLine)
+		score := getScore(plays[0], plays[1])
+		partOneResult += score
+	}
+
+	fmt.Printf("\nPart 1 answer: %d\n", partOneResult)
+
+	var partTwoResult int
+
+	for _, thisLine := range lines {
+		plays := strings.Fields(thisLine)
+		score := choosePlay(plays[0], plays[1])
+		partTwoResult += score
+	}
+
+	fmt.Printf("\nPart 2 answer: %d\n", partTwoResult)
+}
+
+func choosePlay(a, b string) int {
+	var result int
+
+	switch b {
+	case "X":
+		result += getScore(a, loser[a])
+	case "Y":
+		result += getScore(a, a)
+	case "Z":
+		result += getScore(a, winner[a])
+	}
+
+	return result
+}
+
+func getScore(a, b string) int {
+	var result int
+
+	result += scores[b]
+
+	switch {
+	case scores[a] == scores[b]:
+		result += 3
+	case b == winner[a]:
+		result += 6
+	}
+
+	return result
+}
+
+func getInput() ([]string, error) {
+
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	lines := []string{}
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return lines, nil
 }
